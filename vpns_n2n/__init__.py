bab652b25ddc6d9780c6519da3b471fc27595e91
fpemud <fpemud@sina.com>
2017-07-07 21:55:26 +0800

    fix

--- vpns_n2n/__init__.py
+++ vpns_n2n/__init__.py
@@ -167,12 +167,8 @@ class _VirtualBridge:
 
     def on_host_add_or_change(self, source_id, ip_data_dict):
         fn = os.path.join(self.hostsDir, source_id)
+        itemDict = _Util.dnsmasqHostFileToOrderedDict(fn)
         bChanged = False
-
-        itemDict = OrderedDict()
-        with open(fn, "r") as f:
-            for line in f.read().rstrip("\n").split("\n"):
-                itemDict[line.split(" ")[0]] = line.split(" ")[1]
 
         for ip, data in ip_data_dict.items():
             if ip in itemDict:
@@ -189,39 +185,26 @@ class _VirtualBridge:
                     bChanged = True
 
         if bChanged:
-            with open(fn, "w") as f:
-                for ip, hostname in itemDict.items():
-                    f.write(ip + " " + hostname + "\n")
+            _Util.dictToDnsmasqHostFile(itemDict, fn)
             self.dnsmasqProc.send_signal(signal.SIGHUP)
 
     def on_host_remove(self, source_id, ip_list):
         fn = os.path.join(self.hostsDir, source_id)
+        itemDict = _Util.dnsmasqHostFileToOrderedDict(fn)
         bChanged = False
 
-        lineList = []
-        with open(fn, "r") as f:
-            lineList = f.read().rstrip("\n").split("\n")
-
-        lineList2 = []
-        for line in lineList:
-            if line.split(" ")[0] not in ip_list:
-                lineList2.append(line)
-            else:
+        for ip in ip_list:
+            if ip in itemDict:
+                del itemDict[ip]
                 bChanged = True
 
         if bChanged:
-            with open(fn, "w") as f:
-                for line in lineList2:
-                    f.write(line + "\n")
+            _Util.dictToDnsmasqHostFile(itemDict, fn)
             self.dnsmasqProc.send_signal(signal.SIGHUP)
 
     def on_host_refresh(self, source_id, ip_data_dict):
         fn = os.path.join(self.hostsDir, source_id)
-
-        itemDict = dict()
-        with open(fn, "r") as f:
-            for line in f.read().rstrip("\n").split("\n"):
-                itemDict[line.split(" ")[0]] = line.split(" ")[1]
+        itemDict = _Util.dnsmasqHostFileToDict(fn)
 
         itemDict2 = dict()
         for ip, data in ip_data_dict.items():
@@ -229,9 +212,7 @@ class _VirtualBridge:
                 itemDict[ip] = data["hostname"]
 
         if itemDict != itemDict2:
-            with open(fn, "w") as f:
-                for ip, hostname in itemDict2:
-                    f.write(ip + " " + data["hostname"] + "\n")
+            _Util.dictToDnsmasqHostFile(itemDict2, fn)
             self.dnsmasqProc.send_signal(signal.SIGHUP)
 
     def _runCmdServer(self):
@@ -337,3 +318,31 @@ class _Util:
             os.remove(filename)
         elif os.path.isdir(filename):
             shutil.rmtree(filename)
+
+    @staticmethod
+    def dnsmasqHostFileToDict(filename):
+        ret = dict()
+        with open(filename, "r") as f:
+            for line in f.read().split("\n"):
+                if line.startswith("#") or line.strip() == "":
+                    continue
+                t = line.split(" ")
+                ret[t[0]] = t[1]
+        return ret
+
+    @staticmethod
+    def dnsmasqHostFileToOrderedDict(filename):
+        ret = OrderedDict()
+        with open(filename, "r") as f:
+            for line in f.read().split("\n"):
+                if line.startswith("#") or line.strip() == "":
+                    continue
+                t = line.split(" ")
+                ret[t[0]] = t[1]
+        return ret
+
+    @staticmethod
+    def dictToDnsmasqHostFile(filename, ipHostnameDict):
+        with open(filename, "w") as f:
+            for ip, hostname in ipHostnameDict:
+                f.write(ip + " " + hostname + "\n")
